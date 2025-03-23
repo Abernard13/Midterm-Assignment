b60a6cbaab07802444b2791380f8011345afebb8
Alana Bernardez Banegas <bernardezalanis@gmail.com>
2025-03-23 17:56:30 -0500

    update

--- hello_goodbye.cpp
+++ hello_goodbye.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 #include <unistd.h>
 
-void hello_goodbye() {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(1);
-    } else if (pid == 0) {
-        std::cout << "hello\n";
-        exit(0);
-    } else {
-        usleep(10000); // Small delay to ensure child executes first
-        std::cout << "goodbye\n";
-    }
-}
-
 int main() {
-    hello_goodbye();
+    if (fork() == 0) {  // Child process
+        printf("hello\n");
+    } else {  // Parent process
+        sleep(1);  // Small delay to ensure child prints first
+        printf("goodbye\n");
+    }
     return 0;
 }
